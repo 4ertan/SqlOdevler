4a1bc3773fa60a34c3751aa61ec72cc0cf05e104
4ertan <ertanosmanoglu13@gmail.com>
2021-08-28 12:05:14 +0300

    sqlOdev1

---
+++ sqlÖdev.sql
@@ -0,0 +1,9 @@
+1- select title,description from film
+2- select * from film
+where length>60 and length<75
+3- select * from film
+where rental_rate=0.99 and replacement_cost=28.99
+4- select * from customer
+where first_name='Mary'          cevap=Smith
+5- select * from film
+where length <50 and rental_rate=2.99
