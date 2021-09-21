f9fa9215c1f21312514f5c2d0596555232103d49
4ertan <ertanosmanoglu13@gmail.com>
2021-09-21 15:27:12 +0300

    sqlÖdev9.sql
    sql9ödev

---
+++ sqlÖdev9.sql
@@ -0,0 +1,8 @@
+1-select country,city from city
+inner join country on city.country_id=country.country_id;
+
+2- select payment_id,first_name,last_name from customer
+inner join payment on customer.customer_id=payment.customer_id;
+
+3- select rental_id,first_name,last_name from customer
+inner join rental on rental.customer_id=customer.customer_id;
